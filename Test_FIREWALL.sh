3b3b1e0717770f79783b2aae22d52a79cc12236e
Archisman-A <153774587+Archisman-A@users.noreply.github.com>
2025-04-08 10:33:12 +0000

    Update Test_FIREWALL.sh

--- Test_FIREWALL.sh
+++ Test_FIREWALL.sh
@@ -1,4 +1,5 @@
 #This file displays the current status of all the ports which are accepting or being blocked
+#it also checks the ping
 #It also checks the status of port 9050, how is it behaving before/after applying the firewall rules
 
 #!/bin/bash
@@ -6,6 +7,12 @@
 #test current iptables 
 sudo iptables -L
 
-telnet localhost 9050
+#check ping for icmps
+sudo iptables -L -n | grep icmp
 
+telnet localhost 9050
 #telnet ran successfully for the port 9050
+
+
+
+
